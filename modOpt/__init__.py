ffbb27377c94cfb0b5db3ce6d895e37856733089
Bublitz <sassibub@win.tu-berlin.de>
2019-05-14 17:31:20 +0200

    Fix: try to fix __init__.py

--- modOpt/__init__.py
+++ modOpt/__init__.py
@@ -2,8 +2,8 @@
 public functions and classes
 """
 from .model import *
-import .constraints
-import .decompostion
-import .scaling
-import .solver
-import .initialization
+import constraints
+import decompostion
+import scaling
+import solver
+import initialization
