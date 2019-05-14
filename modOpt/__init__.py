a51fca460b47dca6908418c8e6e3e5972ac90816
Bublitz <sassibub@win.tu-berlin.de>
2019-05-14 17:40:58 +0200

    fix: next try

--- modOpt/__init__.py
+++ modOpt/__init__.py
@@ -2,8 +2,8 @@
 public functions and classes
 """
 from .model import *
-import constraints
-import decompostion
-import scaling
-import solver
-import initialization
+import constraints.__init__.py
+#import decompostion
+#import scaling
+#import solver
+#import initialization
