bd06a7a179222b48a01a68048cb16f4ef0f1c882
Saskia Bublitz <saskia.bublitz@tu-berlin.de>
2020-01-23 11:08:40 +0100

    Fix: dll name was correct in version before. Reverted changes

--- modOpt/decomposition/MC33.py
+++ modOpt/decomposition/MC33.py
@@ -4,7 +4,7 @@ Imported Packages
 ***************************************************
 """
 import numpy
-from modOpt.decomposition import MC33
+from modOpt.decomposition import MC33AD
 
 """
 ***************************************************
