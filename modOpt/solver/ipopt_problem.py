1c35c30983586aafee412a5427b8083cad759eaa
Saskia <saskia.bublitz@tu-berlin.de>
2020-12-23 12:45:34 +0100

    Fix: gradient calculation in ipopt for python 3.7

--- modOpt/solver/ipopt_problem.py
+++ modOpt/solver/ipopt_problem.py
@@ -3,26 +3,28 @@ import numpy
 class Ipopt_problem(object):
     def __init__(self, curBlock):
         self.curBlock = curBlock
+        pass
     
     def objective(self, x): 
         self.curBlock.x_tot[self.curBlock.colPerm] = x
         f = self.curBlock.getFunctionValues()
-        m = 0
+        m = 0.0
         
         for fi in f: m += fi**2.0 
              
         return m
-    def gradient(self, x):    
-        grad_m = numpy.zeros(len(x)+1)
+    def gradient(self, x):  
+        self.curBlock.x_tot[self.curBlock.colPerm] = x
+        grad_m = numpy.zeros(len(x))
     
         f = self.curBlock.getFunctionValues()
         jac = self.curBlock.getPermutedJacobian()
     
         for j in range(0, len(x)):
             for i in range(0, len(f)):
-                grad_m[j+1] += 2 * f[i] * jac[i, j]
+                grad_m[j] += 2.0 * f[i] * jac[i, j]
             
         return numpy.array(grad_m, dtype=float)
     
-    def constraints(self, x):
-        return numpy.array([]) 
+   # def constraints(self, x):
+   #     return numpy.array([]) 
